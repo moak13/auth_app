e83fc53fb4adc4ef48a6440098892dfd03149cad
Michael Kalango <michaelkalango@gmail.com>
2020-08-11 01:25:25 +0100

    signup functionality

--- lib/features/signup/view/signup_view.dart
+++ lib/features/signup/view/signup_view.dart
@@ -1,4 +1,6 @@
+import 'package:auth_app/view_models/signup_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
 
 class SignupView extends StatefulWidget {
   SignupView({Key key}) : super(key: key);
@@ -8,8 +10,87 @@ class SignupView extends StatefulWidget {
 }
 
 class _SignupViewState extends State<SignupView> {
+  TextEditingController ctrlFirst = TextEditingController();
+  TextEditingController ctrlLast = TextEditingController();
+  TextEditingController ctrlEmail = TextEditingController();
+  TextEditingController ctrlUsername = TextEditingController();
+  TextEditingController ctrlPassword = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ViewModelBuilder<SignupViewModel>.reactive(
+        builder: (context, model, child) {
+          return Scaffold(
+            body: Container(
+              child: ListView(
+                children: <Widget>[
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                          icon: Icon(Icons.clear),
+                          onPressed: () => model.popPage())),
+                  Text('Sign Up'),
+                  TextField(
+                    controller: ctrlFirst,
+                    decoration: InputDecoration(
+                      labelText: 'Firstname',
+                      hintText: 'firstname',
+                    ),
+                  ),
+                  TextField(
+                    controller: ctrlLast,
+                    decoration: InputDecoration(
+                      labelText: 'Lastname',
+                      hintText: 'lastname',
+                    ),
+                  ),
+                  TextField(
+                    controller: ctrlEmail,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'email',
+                    ),
+                  ),
+                  TextField(
+                    controller: ctrlUsername,
+                    decoration: InputDecoration(
+                      labelText: 'Usename',
+                      hintText: 'username',
+                    ),
+                  ),
+                  TextField(
+                    controller: ctrlPassword,
+                    obscureText: model.isPassword,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'password',
+                      suffixIcon: IconButton(
+                          icon: model.isPassword
+                              ? Icon(Icons.visibility)
+                              : Icon(Icons.visibility_off),
+                          onPressed: () {
+                            model.toggle();
+                          }),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      model.signup(
+                        firstname: ctrlFirst.text,
+                        lastname: ctrlLast.text,
+                        email: ctrlEmail.text,
+                        username: ctrlUsername.text,
+                        password: ctrlPassword.text,
+                      );
+                    },
+                    child: model.isBusy
+                        ? CircularProgressIndicator()
+                        : Text('submit'),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        viewModelBuilder: () => SignupViewModel());
   }
 }
--- lib/view_models/signup_viewmodel.dart
+++ lib/view_models/signup_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 import '../core/utils/result.dart';
 import '../locator.dart';
@@ -6,7 +7,21 @@ import '../models/signup_model.dart';
 import '../services/signup_service.dart';
 
 class SignupViewModel extends BaseViewModel {
-  final signupService = locator<SignupService>();
+  final _signupService = locator<SignupService>();
+  final _navigationService = locator<NavigationService>();
+  final _dialogService = locator<DialogService>();
+
+  bool _isPassword = true;
+  bool get isPassword => _isPassword;
+
+  void popPage() {
+    _navigationService.back();
+  }
+
+  void toggle() {
+    _isPassword = !_isPassword;
+    notifyListeners();
+  }
 
   Future<Result<SignupModel>> signup(
       {String firstname,
@@ -14,14 +29,34 @@ class SignupViewModel extends BaseViewModel {
       String email,
       String username,
       String password}) async {
-    final response = await signupService.performSignup(
+    setBusy(true);
+    final response = await _signupService.performSignup(
         firstname: firstname,
         lastname: lastname,
         email: email,
         username: username,
         password: password);
-    print(response.success);
-    print(response.error);
-    return response;
+    setBusy(false);
+    if (response.success.id != null) {
+      _navigationService.navigateTo('/home_view');
+      // await _dialogService.showDialog(
+      //   title: 'Yay!',
+      //   description: response.success.message,
+      //   buttonTitle: 'OK',
+      // );
+      await _dialogService.showConfirmationDialog(
+        title: 'Yay!',
+        description: response.success.message,
+        cancelTitle: 'OK',
+      );
+      return response;
+    } else {
+      await _dialogService.showDialog(
+        title: 'Error!',
+        description: response.error.message,
+        buttonTitle: 'OK',
+      );
+      return response;
+    }
   }
 }
