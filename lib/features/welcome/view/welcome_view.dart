273e4c46648d771a003a0072469fbda17f7394db
Michael Kalango <michaelkalango@gmail.com>
2020-09-13 23:38:00 +0100

    welcome view fixed

--- lib/features/welcome/view/welcome_view.dart
+++ lib/features/welcome/view/welcome_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stacked/stacked.dart';
 
 import '../../../view_models/welcome_viewmodel.dart';
@@ -9,46 +8,89 @@ class WelcomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double scaleHeight = height / 100;
+    double width = MediaQuery.of(context).size.width;
+    double scaleWidth = width / 100;
+    double sizeText = MediaQuery.of(context).textScaleFactor;
     return ViewModelBuilder<WelcomeViewModel>.reactive(
         builder: (context, model, child) {
-          ScreenUtil.init(
-            context,
-            height: ScreenUtil.screenHeight,
-            width: ScreenUtil.screenWidth,
-            allowFontScaling: true,
-          );
           return Scaffold(
-            body: Container(
-              // height: ScreenUtil().setHeight(ScreenUtil.screenHeight),
-              // width: ScreenUtil().setWidth(ScreenUtil.screenWidth),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Welcome'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          child: RaisedButton(
-                            onPressed: () {
+            body: SafeArea(
+              child: Container(
+                height: height,
+                width: width,
+                color: Colors.white,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Welcome',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: sizeText * 30,
+                        ),
+                      ),
+                      SizedBox(
+                        height: scaleHeight * 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          InkWell(
+                            onTap: () {
                               model.moveToLogin();
                             },
-                            child: Text('Login'),
+                            child: Container(
+                              height: scaleHeight * 7,
+                              width: scaleWidth * 30,
+                              decoration: BoxDecoration(
+                                color: Colors.purple,
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: sizeText * 16,
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                        Expanded(
-                          child: RaisedButton(
-                            onPressed: () {
+                          InkWell(
+                            onTap: () {
                               model.moveToSignup();
                             },
-                            child: Text('Signup'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                            child: Container(
+                              height: scaleHeight * 7,
+                              width: scaleWidth * 30,
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'signup',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: sizeText * 16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
